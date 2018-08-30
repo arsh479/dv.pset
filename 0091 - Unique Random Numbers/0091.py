133cba2c3a483d135384eb501e8d73c3e58186ee
arsh479 <42331307+arsh479@users.noreply.github.com>
2018-08-30 08:58:10 +0530

    0091
    Generate 5 unique random numbers

---
+++ 0091 - Unique Random Numbers/0091.py
@@ -0,0 +1,20 @@
+i = 1
+import random
+
+while i ==1:
+    x =  input('please press enter: ')
+    print
+    if ("" in x) or (" " in x):
+        wow1 = random.randint(0, 10**2)
+        wow2 = random.randint(0, 10**2)
+        wow3 = random.randint(0, 10**2)
+        wow4 = random.randint(0, 10**2)
+        wow5 = random.randint(0, 10**2)
+        print(wow1)
+        print(wow2)
+        print(wow3)
+        print(wow4)
+        print(wow5)
+        print(end = '\n')
+        print('press ctrl + c to exit')
+        print(end = '\n')
