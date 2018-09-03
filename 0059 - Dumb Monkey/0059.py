59f8c1676a1aa612d6da53913fe604b2bb7274c0
arsh479 <42331307+arsh479@users.noreply.github.com>
2018-09-03 16:31:37 +0530

    0059

---
+++ 0059 - Dumb Monkey/0059.py
@@ -0,0 +1,16 @@
+#DUMB MONKEY
+print('                                                             Dumb Monkey')
+i = 1
+
+while i == 1:
+    x = int(input('Please enter pillar height: '))
+    if x>3 and x % 2 !=0:
+        print('yes he can')
+        print('press ctrl + c to exit')
+        print(end='\n')
+        continue
+    else:
+        print('no he cant')
+        print('press ctrl + c to exit')
+        print(end='\n')
+        continue
