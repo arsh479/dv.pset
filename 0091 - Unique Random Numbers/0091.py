1c34f354503dd6a2db3d80128276d89950164744
arsh479 <42331307+arsh479@users.noreply.github.com>
2018-09-06 08:55:14 +0530

    0091

--- 0091 - Unique Random Numbers/0091.py
+++ 0091 - Unique Random Numbers/0091.py
@@ -1,15 +1,20 @@
 i = 1
 import random
+while i == 1:
+    wow1 = random.randint(0, 10**2)
+    wow2 = random.randint(0, 10**2)
+    wow3 = random.randint(0, 10**2)
+    wow4 = random.randint(0, 10**2)
+    wow5 = random.randint(0, 10**2)
 
-while i ==1:
     x =  input('please press enter: ')
-    print
-    if ("" in x) or (" " in x):
-        wow1 = random.randint(0, 10**2)
-        wow2 = random.randint(0, 10**2)
-        wow3 = random.randint(0, 10**2)
-        wow4 = random.randint(0, 10**2)
-        wow5 = random.randint(0, 10**2)
+
+    if (wow1 == wow2) and (wow2 == wow3) and (wow3 == wow4) and (wow4 == wow5)\
+    and (wow1 == wow3) and (wow1 == wow4) and (wow1 == wow5) and (wow2 == wow4) and\
+     (wow2 == wow5) and (wow3 == wow5) and (wow4 == wow5) :
+     continue
+
+    elif ("" in x) :
         print(wow1)
         print(wow2)
         print(wow3)
