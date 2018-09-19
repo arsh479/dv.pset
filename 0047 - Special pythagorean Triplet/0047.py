71883b6922f205b84eb2f7d015305ba11303abd8
arsh479 <42331307+arsh479@users.noreply.github.com>
2018-09-19 16:34:49 +0530

    0047

---
+++ 0047 - Special pythagorean Triplet/0047.py
@@ -0,0 +1,18 @@
+
+a = 1
+b = 1
+c = 1
+for a in range (1,1000):
+
+    for b in range(1, 1000):
+
+        for c in range(1,1000):
+
+            if ((a**2 + b**2) == c**2) and (a+b+c == 1000) and (a<b) and (b<c):
+                print(a)
+                print(b)
+                print(c)
+                break
+                p = a*
+                pp = p*c
+                print(pp)
