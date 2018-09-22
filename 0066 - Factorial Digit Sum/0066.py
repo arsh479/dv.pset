0279272dd4ae2e735fe46cb439deb298e669cff6
arsh479 <42331307+arsh479@users.noreply.github.com>
2018-09-22 18:33:26 +0530

    0066-corrected version

--- 0066 - Factorial Digit Sum/0066.py
+++ 0066 - Factorial Digit Sum/0066.py
@@ -1,9 +1,18 @@
+def findsumofdigits(f):
+    i = 0
+    c = 0
+    strlist = list(str(f))
+    intlist = list(map(int, strlist))
+    for i in range (0, len(str(f))):
+        c = intlist[i] +c
+        i = i+1
+    return (c)
 ls = []
-i = 0
-x = int(input('enter number: '))
-for i in range(0,x): #instead of using the formula n! = n*(n-1) i removed the first part and used (n-i) where i goes from 0 to n-1
-    c = (x-i)# sequence is (100-i1) = 100-0 = 100 , 100-i2 = 100-1 =99...and so on till x which is till i99
-    i = i+1
-    ls.append(c)
-    d = sum(ls)
-print(d)
+i =0
+mul = 1
+x = int(input('Enter a number: '))
+for i in range (0,x):
+    c = (x-i)
+    mul = mul * c
+    f = findsumofdigits(mul)
+print(f)
