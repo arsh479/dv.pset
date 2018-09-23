f38652f841dda283207c724cfe05a8900407202c
arsh479 <42331307+arsh479@users.noreply.github.com>
2018-09-23 15:23:33 +0530

    0063 - corrected

--- 0063 - Power Digit Sum/0063.py
+++ 0063 - Power Digit Sum/0063.py
@@ -1,9 +1,18 @@
-print('enter a number and then the power to find sum of digits in the respective output')
+def findsumofdigits(f):
+    i = 0
+    c = 0
+    strlist = list(str(f))
+    intlist = list(map(int, strlist))
+    for i in range (0, len(str(f))):
+        c = intlist[i] +c
+        i = i+1
+    return (c)
+
+
+print('Enter a number and then the power to find sum of digits in the respective output')
 print(end = '\n')
 x = int(input('Enter a number: '))
 y = int(input('Enter a power: '))
 o = (x**y)
-strls = list(str(o))
-intlist = list(map(int, strls))
-c = sum(intlist)
-print(c)
+sum = findsumofdigits(o)
+print(sum)
